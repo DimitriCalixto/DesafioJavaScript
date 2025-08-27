b92537826ce9e1373c5c5ad2c98bd15cbbb298e7
Dimitri Calixto Arruda Martins <57041642+DimitriCalixto@users.noreply.github.com>
2025-08-27 15:32:04 -0300

    [Create] app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let tittle = document.querySelector('h1');
+tittle.innerHTML = 'Jogo do número secreto';
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = 'Escolha um número entre 1 e 100';
+
+function verificarChute(){
+    
+}
